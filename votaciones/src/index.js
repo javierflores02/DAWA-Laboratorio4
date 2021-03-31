95a65e3901b27c64481f83d4def9a20b43364e46
Javier <javier.flores@tecsup.edu.pe>
2021-03-30 19:44:23 -0500

    Votaciones: Punto 2

--- votaciones/src/index.js
+++ votaciones/src/index.js
@@ -6,9 +6,31 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const handleGood = () => {
+    setGood(good + 1)
+  }
+  const handleNeutral = () => {
+    setNeutral(neutral + 1)
+  }
+  const handleBad = () => {
+    setBad(bad + 1)
+  }
+
   return (
     <div>
-      code here
+      <p>Give feedback</p>
+      <div>
+        <button onClick={handleGood}>good</button>
+        <button onClick={handleNeutral}>neutral</button>
+        <button onClick={handleBad}>bad</button>
+      </div>
+      <p>Statistics</p>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {good+neutral+bad}</p>
+      <p>average {(good*1+neutral*0+bad*-1)/(good+neutral+bad)}</p>
+      <p>positive {(good/(good+neutral+bad))*100}%</p>
     </div>
   )
 }
